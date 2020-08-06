cacdd1e56f6d2ab0c8d919d4c1a749c5dd09d800
dhanush <dhanush@iitg.ac.in>
2020-08-06 19:45:55 +0530

    Updates

---
+++ precache-manifest.925af2d990ccc4c54e90020107278ccd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d626cdd9d66446a632bf0750aab32383",
+    "url": "/covidReact/index.html"
+  },
+  {
+    "revision": "c5a1cbc3a1d973fde9c1",
+    "url": "/covidReact/static/css/main.93d79cc5.chunk.css"
+  },
+  {
+    "revision": "7f29d8df1c29059f32c6",
+    "url": "/covidReact/static/js/2.4ce740e2.chunk.js"
+  },
+  {
+    "revision": "5e9b6f5c16b720e8a86875f97734673e",
+    "url": "/covidReact/static/js/2.4ce740e2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c5a1cbc3a1d973fde9c1",
+    "url": "/covidReact/static/js/main.f47a6a38.chunk.js"
+  },
+  {
+    "revision": "8d9882b6d8e29cdfa92a",
+    "url": "/covidReact/static/js/runtime-main.ef05b4a0.js"
+  }
+]);
