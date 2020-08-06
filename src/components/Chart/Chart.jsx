5e657bd114d55907681bdc479ec2f4661f27cc3d
dhanush <dhanush@iitg.ac.in>
2020-08-06 19:49:10 +0530

    gh-pages

--- src/components/Chart/Chart.jsx
+++ src/components/Chart/Chart.jsx
@@ -1,6 +1,16 @@
 import React, { useState, useEffect } from "react";
+import { fetchDailyData } from "../../api";
 
 const Chart = () => {
+	const [dailyData, setDailyData] = useState({});
+
+	useEffect(() => {
+		const fetchapi = async () => {
+			setDailyData(await fetchDailyData());
+		};
+		fetchapi();
+	}, []);
+	console.log(dailyData);
 	return <h1>Chart</h1>;
 };
 
